01a31b0c41bd0dd4415c9bd07f90a3b621365e9d
bhatara007 <bhatara007@gmail.com>
2020-09-10 01:12:49 +0700

    implement part7 complete

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,8 @@ from django.db import models
 from django.utils import timezone
 
 class Question(models.Model):
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
@@ -12,6 +14,9 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
