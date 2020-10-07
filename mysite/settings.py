38279f2e764a26a424198f3477bdda77be44e98f
Bhatara Chaemchan <bhatara007@gmail.com>
2020-10-07 09:18:34 +0700

    Fixed Travis-Ci and Codecov

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,10 +9,25 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import environ
 
 from pathlib import Path
 from decouple import config
 
+from pathlib import Path
+
+
+env = environ.Env()
+env.read_env()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY', default='my-secret-key')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
