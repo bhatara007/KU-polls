8e49aeb4ad5271d7380578fb814ddef3bccdc4fc
Bhatara Chaemchan <bhatara007@gmail.com>
2020-10-31 16:00:41 +0700

    fix some test docstrings TBA

--- polls/tests/test_voting.py
+++ polls/tests/test_voting.py
@@ -28,28 +28,27 @@ class VotingTests(TestCase):
         User.objects.create_user(username='bhatara007', password='ddddd007')
         self.client.login(username='bhatara007', password='ddddd007')
         self.question = create_question("What is your gender?", pub_date=-4, end_date=5)
-        self.choice1 = Choice(id=1, question=self.question, choice_text="male")
-        self.choice2 = Choice(id=2, question=self.question, choice_text="female")
-        self.choice1.save()
-        self.choice2.save()
+        self.first_choice = Choice(id=1, question=self.question, choice_text="male")
+        self.second_choice = Choice(id=2, question=self.question, choice_text="female")
+        self.first_choice.save()
+        self.second_choice.save()
 
-    def test_authenticated_user_can_replace_thier_vote(self):
-        reverse('polls:vote', args=(self.question.id,)), {'choice': self.choice1.id}
-        self.first_choice = self.question.choice_set.get(pk=self.choice1.id)
-        self.assertEqual(self.choice1.vote_set.all().count(), 1)
-        reverse('polls:vote', args=(self.question.id,)), {'choice': self.choice2.id}
-        self.second_choice = self.question.choice_set.get(pk=self.choice2.id)
-        self.first_choice = self.question.choice_set.get(pk=self.choice1.id)
-        self.assertEqual(self.choice2.vote_set.all().count(), 1)
-        self.assertEqual(self.choice1.vote_set.all().count(), 0)
-
-    def test_previous_vote_show_correctly(self):
-        url = reverse('polls:vote', args=(self.question.id,)), {'choice': self.choice1.id}
-        response1 = self.client.get(url)
-        self.assertContains(response1, self.choice1.choice_text)
-        url = reverse('polls:vote', args=(self.question.id,)), {'choice': self.choice1.id}
-        response2 = self.client.get(url)
-        self.assertNotContains(response2, self.choice1.choice_text)
+    def test_authenticated_user_can_replace_their_vote(self):
+        response = self.client.post(reverse('polls:vote', args=(self.question.id,)), {'choice':self.first_choice.id})
+        self.first_choice = self.question.choice_set.get(pk = self.first_choice.id)
+        self.assertEqual(self.first_choice.vote_set.all().count(), 1)
+        response = self.client.post(reverse('polls:vote', args=(self.question.id,)), {'choice':self.second_choice.id})
+        self.second_choice = self.question.choice_set.get(pk = self.second_choice.id)
+        self.first_choice = self.question.choice_set.get(pk = self.first_choice.id)
+        self.assertEqual(self.second_choice.vote_set.all().count(), 1)
+        self.assertEqual(self.first_choice.vote_set.all().count(), 0)
+        
+    def test_previous_vote_show(self):
+        response1 = self.client.get(reverse('polls:detail', args=(self.question.id,)))
+        self.assertContains(response1, "None")
+        self.client.post(reverse('polls:vote', args=(self.question.id,)), {'choice': self.first_choice.id})
+        response1 = self.client.get(reverse('polls:detail', args=(self.question.id,)))
+        self.assertNotContains(response1, "None")
 
 
 if __name__ == '__main__':
